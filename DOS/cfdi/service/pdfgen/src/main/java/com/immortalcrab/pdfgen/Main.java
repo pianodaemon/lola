582d2b26ed25c3998b7c1bf6b506c4915288942a
ombdev <omontes.dev@gmail.com>
2021-08-29 09:20:17 -0500

    cfdi-utils: Library created. Added Signer and CadenaOriginal classes (#3)
    * numspatrans: Initial commit
    
    * PDF: Created project pdfgen
    
    * POM file slightly changed
    
    * cfdi-utils: Library created. Added Signer and CadenaOriginal classes
    
    * qrcode: Added library

--- DOS/cfdi/service/pdfgen/src/main/java/com/immortalcrab/pdfgen/Main.java
+++ DOS/cfdi/service/pdfgen/src/main/java/com/immortalcrab/pdfgen/Main.java
@@ -2,6 +2,9 @@ package com.immortalcrab.pdfgen;
 
 import com.immortalcrab.cfdi.parser.FacturaParser;
 import com.immortalcrab.numspatrans.Translator;
+import com.immortalcrab.cfdi.utils.CadenaOriginal;
+import com.immortalcrab.cfdi.utils.Signer;
+import com.immortalcrab.qrcode.QRCode;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import org.springframework.util.ResourceUtils;
@@ -24,9 +27,11 @@ public class Main
         String xmlPath = "/home/userd/Downloads/NV139010.XML";
 
         try {
+            // Parse the cfdi file
             FacturaParser fp = new FacturaParser(xmlPath);
             Map<String, Object> ds = fp.getDs();
 
+            // Translate the cfdi total into text (in Spanish)
             var s = (String) ds.get("CFDI_TOTAL");
             String[] a = s.split("\\.");
             String num = Translator.translateIntegerToSpanish(Long.valueOf(a[0])).toUpperCase();
@@ -38,7 +43,38 @@ public class Main
                 num += " PESOS 00/100 M.N.";
             }
             ds.put("CFDI_TOTAL_LETRA", num);
+
+            // Build cadena original
+            String cadenaOriginal = "";
+            try {
+                String cfdiXml = CadenaOriginal.readXml("/home/userd/dev/lola/DOS/cfdi/service/signer-py/5b52aef2-c0a7-4267-9f79-85aaeaddb651.xml");
+                cadenaOriginal = CadenaOriginal.build(cfdiXml, "/home/userd/dev/lola/DOS/cfdi/service/signer-py/cadenaoriginal_3_3.xslt");
+                System.out.println(cadenaOriginal);
+    
+                if (cadenaOriginal.equals("||...||")) {
+                    System.out.println("----------OK-------------");
+                } else {
+                    System.out.println("ERROR!!!!!!!");
+                }
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+
+            // Sign cadena original
+            String privateKeyPemPath = "/home/userd/dev/uploads/CSD_Ecatepec_MOBO8001149UA_20180623_002721.pem";
+            String sello = "";
+            try {
+                sello = Signer.signMessage(privateKeyPemPath, cadenaOriginal);
+                System.out.println(sello);
+
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+            ds.put("SELLO", sello);
+
             System.out.println(ds);
+
+            QRCode.generate("34598foijsdof89uj34oij", 1250, 1250, "/home/userd/output.png");
 
             JasperReport jasperReport = getJasperReport("tq_carta_porte.jrxml");
             // JasperReport jasperReport = getJasperReport("report.jrxml");
