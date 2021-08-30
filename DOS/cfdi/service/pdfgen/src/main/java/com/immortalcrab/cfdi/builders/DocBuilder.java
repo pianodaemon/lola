6593e931f71b19696ecf6ee50237256c2911e07e
Edwin Pĺauchu <pianodaemon@gmail.com>
2021-08-30 09:12:28 -0500

    builder concept

---
+++ DOS/cfdi/service/pdfgen/src/main/java/com/immortalcrab/cfdi/builders/DocBuilder.java
@@ -0,0 +1,6 @@
+package com.immortalcrab.cfdi.builders;
+
+public interface DocBuilder {
+
+    public void buildDoc();
+}
---
+++ DOS/cfdi/service/pdfgen/src/main/java/com/immortalcrab/cfdi/builders/FacturaPdfBuilder.java
@@ -0,0 +1,22 @@
+package com.immortalcrab.cfdi.builders;
+
+import java.util.Map;
+
+public class FacturaPdfBuilder implements DocBuilder {
+
+    private Map<String, Object> ds;
+
+    private FacturaPdfBuilder(final Map<String, Object> ds) {
+        this.ds = ds;
+    }
+
+    static void render(final Map<String, Object> ds) {
+
+        FacturaPdfBuilder ic = new FacturaPdfBuilder(ds);
+        ic.buildDoc();
+    }
+
+    public void buildDoc() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
