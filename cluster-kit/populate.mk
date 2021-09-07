c7f30d65676de3c8739aecc23a2984915cc18a51
Edwin Pĺauchu <pianodaemon@gmail.com>
2021-09-06 23:58:59 -0500

    cfdi image

--- cluster-kit/populate.mk
+++ cluster-kit/populate.mk
@@ -6,15 +6,26 @@ REGISTRY	:= localhost:5000
 
 # Inhouse images for local registry
 SSO_IMG        := $(REGISTRY)/lola/sso:latest
+CFDI_IMG       := $(REGISTRY)/lola/cfdi:latest
 
-all:	sso_push
+all:	sso_push cfdi_push
 
 sso_push:	sso_build
 	(docker push $(SSO_IMG))
+
+cfdi_push:	cfdi_build
+	(docker push $(CFDI_IMG))
 
 sso_build:
 	(cd $(OPERATIONAL)/sso && \
 	docker image build -f Dockerfile . \
 	-t "$(SSO_IMG)")
+
+cfdi_build:
+	(cd $(OPERATIONAL)/cfdi && \
+	docker image build -f Dockerfile . \
+	-t "$(CFDI_IMG)")
+
 clean:
 	- (docker rmi $(SSO_IMG))
+	- (docker rmi $(CFDI_IMG))
