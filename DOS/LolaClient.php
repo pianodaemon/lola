52a39c5e5d2bddc3a6975f5eb7f60e9917f6a3b1
Edwin Plauchu <pianodaemon@gmail.com>
2021-09-29 21:31:59 -0500

    Fact client first POC

--- DOS/LolaClient.php
+++ DOS/LolaClient.php
@@ -1,37 +1,96 @@
 <?php
 
-abstract class LolaClient {
+declare(strict_types=1);
 
-    static $supportedCompanies = array("TIR", "TQ");
+abstract class LolaClient
+{
+    static $supportedCompanies = [
+        "TIR" => [
+            "host" => "lola.tir.uki",
+        ],
+        "TQ" => [
+            "host" => "lola.tq.uki",
+        ],
+    ];
+
+    static $supportedDocs = ["fac"];
     static $userSso = "lola";
     static $passSso = "latrailera";
 
     protected $company;
 
-    function __construct(string &$co) {
-
-        if ( !in_array($co, self::$supportedCompanies) ) {
-
-            $errMsg = "Error: No esta configurada su company para timbrar\r Verifiquelo con el Administrador";
+    function __construct(string $co, string $purpose)
+    {
+        if (!array_key_exists($co, self::$supportedCompanies)) {
+            $errMsg = "Unsupported company by this client";
             throw new Exception($errMsg);
         }
 
-        $this->company= $co;
+        if (!in_array($purpose, self::$supportedDocs)) {
+            $errMsg = "Unsupported document to issue";
+            throw new Exception($errMsg);
+        }
+
+        $this->company = $co;
+        $this->purpose = $purpose;
     }
 
-    abstract public function reqIssue(string &$req);
+    private function logIn(): string
+    {
+        $token = "blabla";
+        if (strlen($token) > 0) {
+            return token;
+        }
+
+        throw new Exception("Login Fail");
+    }
+
+    private function logOut()
+    {
+        return;
+    }
+
+    public function doIssue(string $issueFilePath)
+    {
+        $token = $this->logIn();
+        $this->postTokensDocument($issueFilePath, $token);
+        $this->logOut();
+    }
+
+    private function postTokensDocument(string &$issueFilePath, string &$token)
+    {
+        if (!is_file($issueFilePath)) {
+            $errMsg =
+                "An issue is practically imposible without a request file";
+            throw new Exception($errMsg);
+        }
+
+        $cf = new CURLFile($issueFilePath);
+
+        $ch = curl_init();
+
+        // gear up options previous triggering
+        $reqTemplate = 'http://$host/api/issue/$purpose';
+        $targetUrl = strtr($reqTemplate, [
+            '$host' => self::$supportedCompanies[$this->company]["host"],
+            '$purpose' => $this->purpose,
+        ]);
+
+        curl_setopt($ch, CURLOPT_URL, $targetUrl);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, ["tokensDoc" => $cf]);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $result = curl_exec($ch);
+        curl_close($ch);
+    }
 }
 
-
-class FactClient extends LolaClient {
-
-    public function reqIssue(string &$req) {
-        echo "REQ:" . $req;
-    }
-
-    function __construct(string &$co) {
-
-        parent::__construct($co);
+class FactClient extends LolaClient
+{
+    function __construct(string &$co)
+    {
+        parent::__construct($co, "fac");
     }
 }
 
