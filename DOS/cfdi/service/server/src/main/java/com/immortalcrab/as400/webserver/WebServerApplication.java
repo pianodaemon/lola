aa80a068eb1478760509802e21f38b1765c593ec
Edwin Plauchu <pianodaemon@gmail.com>
2021-09-30 17:29:53 -0500

    Sso cfdi integration (#27)

--- DOS/cfdi/service/server/src/main/java/com/immortalcrab/as400/webserver/WebServerApplication.java
+++ DOS/cfdi/service/server/src/main/java/com/immortalcrab/as400/webserver/WebServerApplication.java
@@ -2,12 +2,31 @@ package com.immortalcrab.as400.webserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.context.annotation.Configuration;
+
 
 @SpringBootApplication
 public class WebServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebServerApplication.class, args);
+    }
 
+    @EnableWebSecurity
+    @Configuration
+    class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable()
+                    .addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+                    .authorizeRequests()
+                    .anyRequest().authenticated();
+        }
+    }
 }
