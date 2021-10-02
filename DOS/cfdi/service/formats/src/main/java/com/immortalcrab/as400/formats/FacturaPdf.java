d4354655e2caa3231b4d904e8b54fa08841ded86
userd <omontes.dev@gmail.com>
2021-10-02 17:58:59 -0500

    formats: Generate sello. cfdi-utils: Read certificado from file

--- DOS/cfdi/service/formats/src/main/java/com/immortalcrab/as400/formats/FacturaPdf.java
+++ DOS/cfdi/service/formats/src/main/java/com/immortalcrab/as400/formats/FacturaPdf.java
@@ -1,11 +1,104 @@
 package com.immortalcrab.as400.formats;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Map;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
+
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+
 import com.immortalcrab.as400.engine.CfdiRequest;
 import com.immortalcrab.as400.engine.Storage;
+import com.immortalcrab.cfdi.utils.CadenaOriginal;
+import com.immortalcrab.cfdi.utils.Signer;
+import com.immortalcrab.numspatrans.Translator;
+import com.immortalcrab.qrcode.QRCode;
+
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import com.immortalcrab.as400.request.FacturaRequest;
+import com.immortalcrab.as400.parser.PairExtractor;
 
 public class FacturaPdf {
 
-    public static void render(final CfdiRequest cfdiReq, Storage st) {
+    public static void main(String[] args) {
+        try {
+            var isr = new InputStreamReader(new FileInputStream("/home/userd/Downloads/NV139360-changed2.txt"), StandardCharsets.UTF_8);
+            var facReq = FacturaRequest.render(PairExtractor.go4it(isr));
+            var template = "/tq_carta_porte.jrxml";
+            var output   = "tq_carta_porte.pdf";
+            render(facReq, template, output);
 
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void render(final CfdiRequest cfdiReq, String pdfTemplate, String output) {
+    // public static void render(final CfdiRequest cfdiReq, Storage st) {
+
+        try {
+            var ds = cfdiReq.getDs();
+            // Translate the cfdi total into text (in Spanish)
+            var s = (String) ds.get("TOTAL");
+            String[] a = s.split("\\.");
+            String num = Translator.translateIntegerToSpanish(Long.valueOf(a[0])).toUpperCase();
+
+            if (a.length > 1) {
+                num += String.format(" PESOS %s/100 M.N.", a[1]);
+            } else {
+                num += " PESOS 00/100 M.N.";
+            }
+            ds.put("TOTAL_LETRA", num);
+            ds.put("UUID", "5b52aef2-c0a7-4267-9f79-85aaeaddb651"); //TODO: hardcode
+            ds.put("FECHSTAMP", "2021-09-28T10:00:00"); //TODO: hardcode
+            ds.put("CDIGITAL_SAT", "00001000000509541499"); //TODO: hardcode
+
+            // Build cadena original
+            String cfdiXml = CadenaOriginal.readXml(
+                    "/home/userd/dev/lola/DOS/cfdi/service/signer-py/5b52aef2-c0a7-4267-9f79-85aaeaddb651.xml");
+            String cadenaOriginal = CadenaOriginal.build(cfdiXml,
+                    "/home/userd/dev/lola/DOS/cfdi/service/signer-py/cadenaoriginal_3_3.xslt");
+            System.out.println(cadenaOriginal);
+
+            // Sign cadena original
+            String privateKeyPemPath = "/home/userd/dev/uploads/CSD_Ecatepec_MOBO8001149UA_20180623_002721.pem";
+            String sello = Signer.signMessage(privateKeyPemPath, cadenaOriginal);
+            System.out.println(sello);
+            ds.put("SELLO", sello);
+
+            // QR Code generation
+            QRCode.generate("34598foijsdof89uj34oij", 1250, 1250, "/home/userd/output.png");
+
+            System.out.println(ds);
+
+            // PDF generation
+            InputStream is = FacturaPdf.class.getResourceAsStream(pdfTemplate);
+            // InputStream is = FacturaPdf.class.getResourceAsStream("/tq_carta_porte.jrxml");
+            JasperReport jasperReport = JasperCompileManager.compileReport(is);
+            JRDataSource conceptos = new JRBeanCollectionDataSource(
+                    (ArrayList<Map<String, String>>) ds.get("CONCEPTOS"));
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, ds, conceptos);
+            JasperExportManager.exportReportToPdfFile(jasperPrint, output);
+            // JasperExportManager.exportReportToPdfFile(jasperPrint, "tq_carta_porte.pdf");
+
+        } catch (IOException ex) {
+            Logger.getLogger(FacturaPdf.class.getName()).log(Level.SEVERE, null, ex);
+
+        } catch (ParserConfigurationException ex) {
+            Logger.getLogger(FacturaPdf.class.getName()).log(Level.SEVERE, null, ex);
+
+        } catch (SAXException ex) {
+            Logger.getLogger(FacturaPdf.class.getName()).log(Level.SEVERE, null, ex);
+
+        } catch (Exception ex) {
+            Logger.getLogger(FacturaPdf.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
--- DOS/cfdi/service/formats/src/main/java/com/immortalcrab/as400/formats/FacturaXml.java
+++ DOS/cfdi/service/formats/src/main/java/com/immortalcrab/as400/formats/FacturaXml.java
@@ -3,7 +3,6 @@ package com.immortalcrab.as400.formats;
 // import java.io.FileInputStream;
 // import java.io.InputStreamReader;
 import java.math.BigDecimal;
-// import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -15,7 +14,12 @@ import com.immortalcrab.as400.engine.CfdiRequest;
 import com.immortalcrab.as400.engine.Storage;
 import com.immortalcrab.as400.error.FormatError;
 import com.immortalcrab.as400.error.StorageError;
+import com.immortalcrab.cfdi.utils.CadenaOriginal;
+import com.immortalcrab.cfdi.utils.Certificado;
+import com.immortalcrab.cfdi.utils.Signer;
+
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
@@ -24,7 +28,6 @@ import javax.xml.bind.JAXBException;
 import javax.xml.datatype.DatatypeConfigurationException;
 // import com.immortalcrab.as400.request.FacturaRequest;
 // import com.immortalcrab.as400.parser.PairExtractor;
-// import java.io.OutputStream;
 
 import mx.gob.sat.cfd._3.ObjectFactory;
 import mx.gob.sat.sitio_internet.cfd.catalogos.CMetodoPago;
@@ -39,7 +42,7 @@ public class FacturaXml {
     //     try {
     //         var isr = new InputStreamReader(new FileInputStream("/home/userd/Downloads/NV139360-changed2.txt"), StandardCharsets.UTF_8);
     //         var facReq = FacturaRequest.render(PairExtractor.go4it(isr));
-    //         render(facReq, System.out);
+    //         render(facReq, null);
     //     } catch (Exception e) {
     //         System.out.println(e);
     //     }
@@ -86,14 +89,11 @@ public class FacturaXml {
             cfdi.setTipoDeComprobante(CTipoDeComprobante.I);
             cfdi.setTotal(new BigDecimal((String) ds.get("TOTAL")));
             cfdi.setMoneda(CMoneda.fromValue((String) ds.get("MONEDA")));
-            cfdi.setCertificado(//TODO: hardcoded
-                    "MIIGAjCCA+qgAwIBAgIUMDAwMDEwMDAwMDA0MTEzMTQzNDcwDQYJKoZIhvcNAQELBQAwggGyMTgwNgYDVQQDDC9BLkMuIGRlbCBTZXJ2aWNpbyBkZSBBZG1pbmlzdHJhY2nDs24gVHJpYnV0YXJpYTEvMC0GA1UECgwmU2VydmljaW8gZGUgQWRtaW5pc3RyYWNpw7NuIFRyaWJ1dGFyaWExODA2BgNVBAsML0FkbWluaXN0cmFjacOzbiBkZSBTZWd1cmlkYWQgZGUgbGEgSW5mb3JtYWNpw7NuMR8wHQYJKoZIhvcNAQkBFhBhY29kc0BzYXQuZ29iLm14MSYwJAYDVQQJDB1Bdi4gSGlkYWxnbyA3NywgQ29sLiBHdWVycmVybzEOMAwGA1UEEQwFMDYzMDAxCzAJBgNVBAYTAk1YMRkwFwYDVQQIDBBEaXN0cml0byBGZWRlcmFsMRQwEgYDVQQHDAtDdWF1aHTDqW1vYzEVMBMGA1UELRMMU0FUOTcwNzAxTk4zMV0wWwYJKoZIhvcNAQkCDE5SZXNwb25zYWJsZTogQWRtaW5pc3RyYWNpw7NuIENlbnRyYWwgZGUgU2VydmljaW9zIFRyaWJ1dGFyaW9zIGFsIENvbnRyaWJ1eWVudGUwHhcNMTgwNjIzMDEwMDQ0WhcNMjIwNjIzMDEwMDQ0WjCBojEcMBoGA1UEAxQTT01BUiBNT05URVMgQlJJU0XRTzEcMBoGA1UEKRQTT01BUiBNT05URVMgQlJJU0XRTzEcMBoGA1UEChQTT01BUiBNT05URVMgQlJJU0XRTzEWMBQGA1UELRMNTU9CTzgwMDExNDlVQTEbMBkGA1UEBRMSTU9CTzgwMDExNEhERk5STTA5MREwDwYDVQQLEwhFY2F0ZXBlYzCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAN/fKY47sz4iqaUcxpWBzBaa7hNkFbPb09FT8kECtUfEsK3Wqp9/sB/CVokBlt7AFRYIhQBzzqgwA5+3IkPjUy+N15qLJlAyyPA7aHJljMmmsa9QNtyoknk9BMhPTOrORiUoXppSdqxe9HvlN/KqESsvXiRyLN7aiLaJmau0rknnz+OUHkWmtZLSwSMdVfoa2iVGszCuV492Ls59BdP0b0lp2xiJetSwHhgZYvsXJh3TIP5rN1OLhr75vIP3sbeaYDa48PkjE3xDWdOTi6/DyOtIqWosw2O9l65WbVVPZJ2cfvo9YtjgGuH9YGH98nJDMq6yvUxShoqzrsesbaBgyT8CAwEAAaMdMBswDAYDVR0TAQH/BAIwADALBgNVHQ8EBAMCBsAwDQYJKoZIhvcNAQELBQADggIBAGHpqpX8gBueUlcy4f0/k2iWDH1ACMvBWsTLnSWjfl4Fuje70VWJ40LuvYdGYET1ri7JnJJLca7YVK3Wc3HKxY2V64mf8c4LG75mnih/QQJwTOWPBX1zhAxkfHljA0Bl1C5kp+DpsMN4N+o70AKFRLkr5lJ88N/hKX08NyLbLDe0B0zUOygsDaEKlWWzWBI5Bq7CjM/Hi+RfXSUwwTf24yl7ofbeYBoxO4///3ddg4Sgl1L7oLFbLHSWqrmqUpzDMMoy8ycTh894+ATsZno+XvgE+i6QLaxXjE4B8DFl0Scn4pHptxN8fg7EFeuh0ll7edeYUW+BMiRI2JgP5S54kcyRdnyUEoFwf5nxKQEApbqZTv2QtEEZ5wUiL4iSkZ7iFa4nwtLIONJecwAhaadIhGC9RwO7PdwOc/85aElRdMJOuL6OczHc8gpY5rpirneyskopwtsmzsJqpwOHMWOzO8S8FcZEUGoMG87ujgsXQGvc7nMXEd++5ZtXmyAKT0OrXeRQNaTHbZn0KfdkvXvEUgSMqKwVr71uZg+dzGVNJGvRY4uRwYCi296kOjHpg5BgmJjSSgzLSBaH9RHDZOQMy7e3wV+H9i3BOIfx1VtoQmU9KBKXvszTsYXTZm19s89yYFXKEBghc0AcgjhawfvZhQ24t0ha2SL7sIksQ1/nsU+M");
+            cfdi.setCertificado(Certificado.readFromFile("/resources/TQU740415380.cer"));
             cfdi.setSubTotal(new BigDecimal((String) ds.get("SUBTOT")));
             cfdi.setCondicionesDePago((String) ds.get("CONPAG"));
             cfdi.setNoCertificado((String) ds.get("CDIGITAL"));
             cfdi.setFormaPago(((String) ds.get("FORPAG")).split(":")[0]);
-            cfdi.setSello(//TODO: hardcoded
-                    "StQ+XBUuN23YX1w5u1NGQMA7pewppUL88ZkiKnNKo+Mo2fEaoSXjIwMNoFREKyXX4NH9SiljdNaVi0bYFdDyFanezMAOzysuL2/2krQTWTWcujANVaHcLFPZ2nBE43BVRrekuo5GfIomD9nCR3j7c336KV5CIJa1vSPJpv+LTx/mkiNW3iR4/vxSIOzAreioewAfnbGqe7r00R14XxxpqIjb4cUg7b9e1t/xVlNtieJFX5iRtVrgV3jFi76JJJQKF+JupDa/gd/MV+u4KweFlDmJ6qIKMchEkTtm7k4SO8WnPVcSYEJloQigauC9EcOs2W1HJOxX44u5FwGPH0Rrlg==");
             cfdi.setVersion("3.3");
             cfdi.setFecha(DatatypeFactory.newInstance().newXMLGregorianCalendar((String) ds.get("FECHOR")));
 
@@ -189,8 +189,17 @@ public class FacturaXml {
             marshaller.setProperty("jaxb.formatted.output", true);
             marshaller.marshal(cfdi, sw);
 
+            var cadenaOrig = CadenaOriginal.build(sw.toString(), "/resources/cadenaoriginal_3_3.xslt");
+            var sello = Signer.signMessage("/resources/TQU740415380.pem", cadenaOrig);
+            cfdi.setSello(sello);
+            marshaller.marshal(cfdi, sw);
+
         } catch (JAXBException | DatatypeConfigurationException ex) {
-            throw new FormatError("An error ocurried when forming the factura xml", ex);
+            throw new FormatError("An error occurred when forming the factura xml", ex);
+        } catch (IOException ex) {
+            throw new FormatError("An error occurred when reading Certificado from file", ex);
+        } catch (Exception ex) {
+            throw new FormatError("An error occurred when building Cadena Original", ex);
         }
 
         return sw;
