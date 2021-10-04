86081ba4a93c5eeea853a35dc89644dfa0ef260e
userd <omontes.dev@gmail.com>
2021-10-03 20:47:27 -0500

    engine: Fix to StepPdf.render() throwing errors

--- DOS/cfdi/service/engine/src/main/java/com/immortalcrab/as400/engine/StepPdf.java
+++ DOS/cfdi/service/engine/src/main/java/com/immortalcrab/as400/engine/StepPdf.java
@@ -1,6 +1,9 @@
 package com.immortalcrab.as400.engine;
 
+import com.immortalcrab.as400.error.FormatError;
+import com.immortalcrab.as400.error.StorageError;
+
 public interface StepPdf {
 
-    public void render(final CfdiRequest cfdiReq, Storage st);
+    public void render(final CfdiRequest cfdiReq, Storage st) throws FormatError, StorageError;
 }
